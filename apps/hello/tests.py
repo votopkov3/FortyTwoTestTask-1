2dea72a6f1906c260a0fa8119eb092f90a2a0f17
votopkov <votopkov.webmaster@gmail.com>
2016-01-27 22:39:27 +0200

    ticket_1: get test from the previous test and apapt it here

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,9 +1,107 @@
+# -*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
+from django.test import Client
 from django.test import TestCase
+from models import Profile
+from django.utils.encoding import smart_unicode
 
-# Create your tests here.
+client = Client()
 
 
-class SomeTests(TestCase):
-    def test_math(self):
-        "put docstrings in your tests"
-        assert(2 + 2 == 5)
+class ProfileMethodTests(TestCase):
+
+    def setUp(self):
+        Profile.objects.create(name=u"Владимир", last_name=u"Отопков")
+        Profile.objects.create(name=u"Василий", last_name=u"Петров")
+        # get main page
+        self.response = self.client.get(reverse('hello:index'))
+
+    def test_enter_main_page(self):
+        """
+        Test entering main page
+        """
+        # if index page exists
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_profile(self):
+        """
+        Testing profile shown in the page
+        """
+        # get profile
+        profile = Profile.objects.first()
+        # test context main view
+        self.assertEqual(self.response.context['profile'], profile)
+        # test profile data exist on the main page
+        self.assertContains(self.response, u'Отопков')
+        self.assertContains(self.response, u'Владимир')
+
+    def test_non_another_profile(self):
+        """
+        Test if exist another profile in the page
+        """
+        # test if not another profile on index
+        self.assertNotEqual(self.response.context['profile'],
+                            Profile.objects.get(id=2))
+        self.assertNotIn('Василий', self.response.content)
+
+    def test_unicode(self):
+        """
+        Test unicode data on the page
+        """
+        profile = Profile.objects.first()
+        self.assertEqual(smart_unicode(profile), u'Отопков')
+
+    def test_db_entries_count(self):
+        """
+        Test db entries
+        """
+        profile = Profile.objects.all().count()
+        # one profile in fixtures and one in setUp
+        self.assertEqual(profile, 2)
+
+    def test_admin(self):
+        response = self.client.get(reverse('admin:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_login(self):
+        admin = {'name': 'admin',
+                 'password': 'admin'}
+        response = self.client.post(reverse('admin:index'), admin)
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_html(self):
+        response = self.client.get(reverse('task:index'))
+        self.assertTrue('<h1>42 Coffee Cups Test Assignment</h1>'
+                        in response.content)
+
+
+class ProfileNoDataMethodTests(TestCase):
+
+    def setUp(self):
+        # get main page
+        self.response = self.client.get(reverse('task:index'))
+
+    def test_enter_main_page(self):
+        """
+        Test entering main page
+        """
+        # if index page exists
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_profile(self):
+        """
+        Testing profile shown in the page
+        """
+        # get profile
+        profile = Profile.objects.all().count()
+        self.assertEqual(profile, 0)
+        # test profile data exist on the main page
+        self.assertNotContains(self.response, u'Отопков')
+        self.assertNotContains(self.response, u'Владимир')
+
+    def test_db_entries_count(self):
+        """
+        Test db entries
+        """
+        profile = Profile.objects.all().count()
+        self.assertEqual(profile, 0)
