978b06c2701f7a83d0db4fc0dc917f69ab8bcf7e
votopkov <votopkov.webmaster@gmail.com>
2016-01-31 17:40:13 +0200

    ticket:8 update models, tests

--- apps/hello/middleware.py
+++ apps/hello/middleware.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -* -
 from models import Requests
-from django.utils import timezone as t
 
 
 class SaveHttpRequestMiddleware(object):
@@ -9,6 +8,5 @@ class SaveHttpRequestMiddleware(object):
         if request.is_ajax():
             return None
         save_request = Requests(request=request,
-                                pub_date=t.now() + t.timedelta(hours=3),
                                 path=request.build_absolute_uri())
         return save_request.save()
