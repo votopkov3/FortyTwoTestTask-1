20180e767244acbe0066fa24876dae320e4ad465
Volodymyr <young_smith@mail.ru>
2016-02-25 18:01:34 +0200

    ticket:3 divide test on files

--- apps/hello/tests/tests_middleware.py
+++ apps/hello/tests/tests_middleware.py
@@ -2,14 +2,17 @@ from django.core.urlresolvers import reverse
 from django.test import RequestFactory
 from apps.hello.middleware import SaveHttpRequestMiddleware
 from apps.hello.models import Requests
+from django.test import TestCase
 
 
-def test_save_request(self):
+class MiddleWareTests(TestCase):
+
+    def test_save_request(self):
         """
         Test SaveHttpRequestMiddleware()
         """
-        # test if 12 request in db
-        self.assertEqual(Requests.objects.count(), 12)
+        # test if 10 request in db
+        self.assertEqual(Requests.objects.count(), 10)
         # create client and savehttpr... instance
         self.save_http = SaveHttpRequestMiddleware()
         self.new_request = RequestFactory().get(reverse(
@@ -17,4 +20,4 @@ def test_save_request(self):
         # save request to DB
         self.save_http.process_request(request=self.new_request)
         # test saving request to DB
-        self.assertEqual(Requests.objects.count(), 13)
+        self.assertEqual(Requests.objects.count(), 11)
