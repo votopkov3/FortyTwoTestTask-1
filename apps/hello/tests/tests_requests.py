7db9d4b0615439d6422c4a45f41d9b1bd69e918e
Volodymyr <young_smith@mail.ru>
2016-02-26 23:40:18 +0200

    ticket:3 update tests

--- apps/hello/tests/tests_requests.py
+++ apps/hello/tests/tests_requests.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
+from django.core import serializers
 from django.core.urlresolvers import reverse
 from django.test import Client
 from django.test import TestCase
@@ -33,9 +34,11 @@ class SaveHttpRequestTests(TestCase):
         """
         Testing request list view function
         """
+        # delete all requests
+        Requests.objects.all().delete()
         # create new 10 requests will be 12 requests in db
         i = 0
-        while i < 10:
+        while i <= 10:
             Requests.objects.create(
                 request='request_1',
                 path='/'
@@ -58,8 +61,14 @@ class SaveHttpRequestTests(TestCase):
         # get json response and loads it
         response_list = json.loads(response.content)
         # test if 10 requests in response
-        resp_list_count = sum(1 for x in response_list)
-        self.assertEqual(resp_list_count, 10)
+        self.assertEqual(len(response_list), 10)
+
+        # test queryset
+        requests = serializers.serialize(
+            "json",
+            Requests.objects.filter(id__gt=0).reverse()[:10]
+        )
+        self.assertEqual(response.content, requests)
 
     def test_last_requests(self):
         """
