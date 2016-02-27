89d2d7485e77a8cad0bba268a2d6d3b1f8d23a81
Volodymyr <young_smith@mail.ru>
2016-02-27 13:59:41 +0200

    ticket:8 fix errors

--- apps/hello/management/commands/model_list.py
+++ apps/hello/management/commands/model_list.py
@@ -1,13 +1,33 @@
-from django.core.management.base import NoArgsCommand
+from sys import stderr, stdout
+from django.core.management.base import BaseCommand
 from django.db.models import get_models
+from optparse import make_option
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
 
-    help = 'Print all project models and the count of objects in every model'
+    option_list = BaseCommand.option_list + (
+        make_option('--stdout',
+                    action='store_true',
+                    dest='stdout',
+                    default=False,
+                    help='output to STDOUT'),
+        make_option('--stderr',
+                    action='store_true',
+                    dest='stderr',
+                    default=False,
+                    help='Output to STDERR'),
+    )
 
-    def handle(self, *args, **kwargs):
-        for model in get_models():
-            self.stderr.write("Error: %s.%s %d\n" %
-                              (model.__module__, model.__name__,
-                               model.objects.count()))
+    def handle(self, *args, **options):
+
+        if options['stdout']:
+            for model in get_models():
+                stdout.write("%s.%s %d\n" %
+                             (model.__module__, model.__name__,
+                              model.objects.count()))
+        if options['stderr']:
+            for model in get_models():
+                stderr.write("Error: %s.%s %d\n" %
+                             (model.__module__, model.__name__,
+                              model.objects.count()))
---
+++ model_list.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python manage.py model_list --stderr 2>> $(date +"%Y-%m-%d").dat
