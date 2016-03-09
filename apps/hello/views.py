0bea2bc8756f476a6476b18c490b791a8f5c56c9
Volodymyr <young_smith@mail.ru>
2016-03-09 12:20:31 +0200

    ticket:5 merge to master

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.decorators import login_required
 from django.core import serializers
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.views.decorators.http import require_POST
 from apps.hello.forms import ProfileForm
 from models import Profile
 from models import Requests
@@ -23,7 +22,24 @@ def main(request):
 
 @login_required()
 def edit_profile(request):
-    profile = Profile.objects.first()
+    identify = request.POST.get('id', 1)
+    profile = Profile.objects.get(id=identify)
+    form = ProfileForm(request.POST, request.FILES, instance=profile)
+    logger.info(form)
+    if request.POST:
+        if form.is_valid():
+            form.save()
+            profile = Profile.objects.get(id=int(identify))
+            profile_to_json = {'status': "success",
+                               'image_src': profile.photo.url
+                               if profile.photo else ''}
+        else:
+            profile_to_json = {'status': "error",
+                               'image_src': profile.photo.url
+                               if profile.photo else ' '}
+
+        return HttpResponse(json.dumps(profile_to_json),
+                            content_type="application/json")
     user_form = ProfileForm(instance=profile)
     context = dict(profile=profile, user_form=user_form)
     return render(request, 'hello/edit_profile.html', context)
@@ -40,25 +56,3 @@ def request_list(request):
     requests = Requests.objects.all()[:10]
     context = {'requests': requests}
     return render(request, 'hello/request_list.html', context)
-
-
-@login_required()
-@require_POST
-def update_profile(request):
-    identify = request.POST.get('id')
-    profile = Profile.objects.get(id=identify)
-    form = ProfileForm(request.POST, request.FILES, instance=profile)
-    logger.info(form)
-    if form.is_valid():
-        form.save()
-        profile = Profile.objects.get(id=int(identify))
-        profile_to_json = {'status': "success",
-                           'image_src': profile.photo.url
-                           if profile.photo else ''}
-    else:
-        profile_to_json = {'status': "error",
-                           'image_src': profile.photo.url
-                           if profile.photo else ' '}
-
-    return HttpResponse(json.dumps(profile_to_json),
-                        content_type="application/json")
