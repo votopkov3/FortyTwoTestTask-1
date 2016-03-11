96c877bff4d65f94dbf54338f84e05cefa44e01a
Volodymyr <young_smith@mail.ru>
2016-03-11 23:21:31 +0200

    ticket:8 update signals

--- apps/hello/signals.py
+++ apps/hello/signals.py
@@ -8,11 +8,10 @@ exclude_models_name = ['LogEntrry', 'ContentType',
 
 @receiver(post_save)
 def post_save_signal(sender, created, **kwargs):
-    if created and sender.__name__ not in exclude_models_name:
-        LogEntrry.objects.create(title=sender.__name__, status='Create')
-    elif not created:
-        LogEntrry.objects.create(title=sender.__name__, status='Update')
-    return
+    if created and sender.__name__ in exclude_models_name:
+        return
+    status = 'Create' if created else 'Update'
+    LogEntrry.objects.create(title=sender.__name__, status=status)
 
 
 @receiver(post_delete)
